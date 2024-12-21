73c2a43c981603c3e56d54e3a8c2f2fd1ce42a69
aslhanuznx <asliuzun3461@gmail.com>
2024-12-21 20:08:27 +0300

    düzenleme

--- admin/index.php
+++ admin/index.php
@@ -20,6 +20,8 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
   <!-- Theme style -->
   <link rel="stylesheet" href="dist/css/adminlte.min.css">
+  <!-- style --> 
+  <link rel="stylesheet"  href="style.css">
 </head>
 <body class="hold-transition sidebar-mini">
 <div class="wrapper">
