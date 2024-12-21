450a24dfc86f89290d2e9cd5b4366ffc5e0aa548
aslhanuznx <asliuzun3461@gmail.com>
2024-12-22 00:16:52 +0300

    sidebar düzenlendi

--- admin/sidebar.php
+++ admin/sidebar.php
@@ -1,32 +1,14 @@
-<!-- Main Sidebar Container -->
-<aside class="main-sidebar sidebar-dark-primary elevation-4">
-    <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
-      <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">AdminLTE 3</span>
-    </a>
-
+ <!-- Main Sidebar Container -->
+ <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Sidebar -->
     <div class="sidebar">
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+          <img src="/customers.png" class="img-circle elevation-2" ">
         </div>
         <div class="info">
-          <a href="#" class="d-block">Alexander Pierce</a>
-        </div>
-      </div>
-
-      <!-- SidebarSearch Form -->
-      <div class="form-inline">
-        <div class="input-group" data-widget="sidebar-search">
-          <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
-          <div class="input-group-append">
-            <button class="btn btn-sidebar">
-              <i class="fas fa-search fa-fw"></i>
-            </button>
-          </div>
+          <a href="#" class="d-block">ADMİN PANELİ</a>
         </div>
       </div>
 
@@ -35,39 +17,236 @@
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
-          <li class="nav-item menu-open">
-            <a href="#" class="nav-link active">
-              <i class="nav-icon fas fa-tachometer-alt"></i>
+
+               <li class="nav-item">
+            <a href="pages/widgets.html" class="nav-link">
+            <i class="fa-solid fa-store"></i>
               <p>
-                Starter Pages
+                KAFEMİZ
+              </p>
+            </a>
+          </li>
+
+           <!-- Müşteri yönetimi -->
+           <li class="nav-item">
+            <a href="#" class="nav-link">
+            <i class="fa-solid fa-users"></i>
+              <p>
+                Müşteri Yönetimi
                 <i class="right fas fa-angle-left"></i>
               </p>
             </a>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="#" class="nav-link active">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Active Page</p>
+                <a href="pages/charts/chartjs.html" class="nav-link">
+                <i class="fa-sharp-duotone fa-solid fa-user"></i>
+                  <p>Müşteri Bilgileri</p>
                 </a>
               </li>
               <li class="nav-item">
-                <a href="#" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Inactive Page</p>
+                <a href="pages/charts/flot.html" class="nav-link">
+                <i class="fa-solid fa-tag"></i>
+                  <p>indirimler</p>
+                </a>
+              </li>
+</ul>
+         <!-- Menü yönetimi -->
+          <li class="nav-item menu-open">
+            <a href="#" class="nav-link active">
+            <i class="fa-solid fa-utensils"></i>
+              <p>
+              Menü Yönetimi
+                <i class="right fas fa-angle-left"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="./index.html" class="nav-link active">
+                <i class="fa-solid fa-notes-medical"></i>
+                  <p>Menü Ekle</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="./hakkimizda" class="nav-link">
+                <i class="fa-solid fa-rotate-right"></i>
+                  <p>Menü Güncelle</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="./index3.html" class="nav-link">
+                <i class="fa-solid fa-square-minus"></i>
+                  <p>Menü Sil </p>
                 </a>
               </li>
             </ul>
           </li>
+
+          <!-- Sipariş yönetimi -->
           <li class="nav-item">
             <a href="#" class="nav-link">
-              <i class="nav-icon fas fa-th"></i>
+            <i class="fa-solid fa-pen-to-square"></i>
               <p>
-                Simple Link
-                <span class="right badge badge-danger">New</span>
+                Sipariş Yönetimi
+                <i class="fas fa-angle-left right"></i>
+               
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="pages/layout/top-nav.html" class="nav-link">
+                <i class="fa-solid fa-check"></i>
+                  <p>Aktif Siparişler</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="pages/layout/top-nav-sidebar.html" class="nav-link">
+                <i class="fa-solid fa-receipt"></i>
+                  <p>Sipariş Durumu</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="pages/layout/boxed.html" class="nav-link">
+                <i class="fa-solid fa-file-export"></i>
+                  <p>Geçmiş Siparişler</p>
+                </a>
+              </li>
+            </ul>
+          </li>
+         
+             <!-- FİNANS YÖNETİMİ -->
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+            <i class="fa-solid fa-coins"></i>
+              <p>
+              Gelir ve Finans Yönetimi
+                <i class="fas fa-angle-left right"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="pages/UI/general.html" class="nav-link">
+                <i class="fa-solid fa-circle-dollar-to-slot"></i>
+                  <p>Satış Raporları</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="pages/UI/icons.html" class="nav-link">
+                  <i class="fa-solid fa-chart-line"></i>
+                  <p>Gelir-Gider Takibi</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="pages/UI/buttons.html" class="nav-link">
+                <i class="fa-solid fa-cash-register"></i>
+                  <p>Kasa Yönetimi</p>
+                </a>
+              </li>
+</ul>
+
+            <!-- ÇALIŞAN KADROMUZ -->
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-edit"></i>
+              <p>
+                Çalışan Yönetimi
+                <i class="fas fa-angle-left right"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="pages/forms/general.html" class="nav-link">
+                <i class="fa-solid fa-person"></i>
+                  <p>Çalışan Bilgileri</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="pages/forms/advanced.html" class="nav-link">
+                <i class="fa-solid fa-clock"></i>
+                  <p>İzin ve Çalışma Saatleri</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="pages/forms/editors.html" class="nav-link">
+                <i class="fa-solid fa-money-bill"></i>
+                  <p>Maaş ve Ödeme Yönetimi</p>
+                </a>
+              </li>
+          </li>
+</ul>
+            <!-- İŞLETME BİLGİLERİ -->
+             
+          <li class="nav-header"><br><br>İŞLETME BİLGİLERİ</li>
+          <li class="nav-item">
+            <a href="pages/calendar.html" class="nav-link">
+            <i class="fa-solid fa-user-plus"></i>
+              <p>
+                Kadromuz
+              </p>
+            </a>
+          </i>
+
+          
+          <li class="nav-item">
+            <a href="pages/calendar.html" class="nav-link">
+            <i class="fa-brands fa-slack"></i>
+                Vizyonumuz
               </p>
             </a>
           </li>
-        </ul>
+
+
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+            <i class="fa-regular fa-file-lines"></i>
+              <p>
+                Rezervasyon Ve Etkinliklerimiz
+                <i class="fas fa-angle-left right"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="pages/mailbox/mailbox.html" class="nav-link">
+                <i class="fa-regular fa-file"></i>
+                  <p>Rezervasyon Takibi</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="pages/mailbox/compose.html" class="nav-link">
+                <i class="fa-brands fa-waze"></i><i class="fa-brands fa-waze"></i>
+                  <p>Etkinliklerimiz</p>
+                </a>
+              </li>
+</ul>
+              <li class="nav-item">
+            <a href="#" class="nav-link">
+            <i class="fa-regular fa-face-smile"></i>
+              <p>
+                Müşteri Memnuniyeti
+                <i class="fas fa-angle-left right"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="pages/mailbox/mailbox.html" class="nav-link">
+                <i class="fa-solid fa-comment"></i>
+                  <p>Müşteri Yorumları</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="pages/mailbox/compose.html" class="nav-link">
+                <i class="fa-solid fa-bolt"></i>
+                  <p>Müşteri Şikayetleri</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="pages/mailbox/read-mail.html" class="nav-link">
+                <i class="fa-solid fa-bell"></i>
+                  <p>Müşteri Önerileri</p>
+                </a>
+              </li>
+            </ul>
+          </li>
+</ul>       
       </nav>
       <!-- /.sidebar-menu -->
     </div>
