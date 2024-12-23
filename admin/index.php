a2d26f72795c65fa39211e97fdde290a4397bf85
aslhanuznx <asliuzun3461@gmail.com>
2024-12-23 23:52:03 +0300

    sidebara sayfaalr eklendi

--- admin/index.php
+++ admin/index.php
@@ -12,6 +12,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>PHP PDO || Admin Panel </title>
 
+ 
    <!-- Font Awesome CDN -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
   <!-- Google Font: Source Sans Pro -->
@@ -22,6 +23,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <link rel="stylesheet" href="dist/css/adminlte.min.css">
   <!-- style --> 
   <link rel="stylesheet"  href="style.css">
+
 </head>
 <body class="hold-transition sidebar-mini">
 <div class="wrapper">
@@ -33,6 +35,27 @@ scratch. This page gets rid of all links and provides the needed markup only.
   include_once "controlsidebar.php";
   include_once "jquery.php";
   include_once "footer.php";
+  include_once "müşteribilgileri.php";
+  include_once "İndirimler.php";
+  include_once "menüekle.php";
+  include_once "menügüncelle.php";
+  include_once "menüsil.php";
+  include_once "aktifsip.php";
+  include_once "sipdurumu.php";
+  include_once "geçmişsip.php";
+  include_once "satışraporu.php";
+  include_once "gelirgider.php";
+  include_once "kasa.php";
+  include_once "çalışanbil.php";
+  include_once "çalışmasaati.php";
+  include_once "maaş.php";
+  include_once "kadro.php";
+  include_once "vizyon.php";
+  include_once "rezervasyon.php";
+  include_once "etkinlik.php";
+  include_once "yorum.php";
+  include_once "sikayet.php";
+  include_once "öneri.php";
   ?>
 
   <div class="container">
